af3f97c7441c38c38a27828d372c705fff867259
Ricardo Gerardi <ricardo.gerardi@gmail.com>
2026-01-02 00:12:10 -0500

    Revert "Moving base to aurora-dx"
    This reverts commit 98017c7d47d6a227afe57d33bd99f035d29ce880.

--- build_files/build.sh
+++ build_files/build.sh
@@ -13,7 +13,7 @@ dnf5 remove -y firefox.x86_64 firefox-langpacks.x86_64
 
 # this installs a package from fedora repos
 dnf5 install -y tmux 
-#dnf5 group install -y --with-optional virtualization
+dnf5 group install -y --with-optional virtualization
 
 # Use a COPR Example:
 #
@@ -23,14 +23,13 @@ dnf5 install -y tmux
 # dnf5 -y copr disable ublue-os/staging
 
 dnf5 copr -y enable solopasha/hyprland
-#dnf5 copr -y enable ublue-os/packages
+dnf5 copr -y enable ublue-os/packages
 
-dnf5 -y install hyprland hyprpaper hyprlock hypridle hyprpolkitagent chafa foot foot-terminfo dunst waybar wl-clipboard nwg-bar nwg-look imv wev zsh zsh-autosuggestions zsh-syntax-highlighting cascadia-code-nf-fonts cascadia-mono-nf-fonts google-noto-sans-fonts google-noto-sans-mono-fonts google-noto-serif-fonts guestfs-tools rclone socat vulkan-tools borgbackup fontawesome-fonts-all
+dnf5 -y install hyprland hyprpaper hyprlock hypridle hyprpolkitagent chafa foot foot-terminfo dunst waybar wl-clipboard nwg-bar nwg-look imv wev zsh zsh-autosuggestions zsh-syntax-highlighting SDL2_image SDL3_image SDL3_ttf breeze-cursor-theme breeze-gtk-common breeze-gtk-gtk3 breeze-gtk-gtk4 breeze-icon-theme incus incus-agent f43-backgrounds-base fastfetch fprintd fprintd-pam gdisk glow cascadia-code-nf-fonts cascadia-mono-nf-fonts google-noto-sans-fonts google-noto-sans-mono-fonts google-noto-serif-fonts guestfs-tools gum iwd lm_sensors parted podman-compose podman-machine podman-tui powertop rclone socat tailscale tuned tuned-ppd ublue-brew ublue-os-libvirt-workarounds vulkan-tools xwaylandvideobridge borgbackup fontawesome-fonts-all
 
-##dnf5 -y install hyprland hyprpaper hyprlock hypridle hyprpolkitagent chafa foot foot-terminfo dunst waybar wl-clipboard nwg-bar nwg-look imv wev zsh zsh-autosuggestions zsh-syntax-highlighting SDL2_image SDL3_image SDL3_ttf breeze-cursor-theme breeze-gtk-common breeze-gtk-gtk3 breeze-gtk-gtk4 breeze-icon-theme incus incus-agent f43-backgrounds-base fastfetch fprintd fprintd-pam gdisk glow cascadia-code-nf-fonts cascadia-mono-nf-fonts google-noto-sans-fonts google-noto-sans-mono-fonts google-noto-serif-fonts guestfs-tools gum iwd lm_sensors parted podman-compose podman-machine podman-tui powertop rclone socat tailscale tuned tuned-ppd ublue-brew ublue-os-libvirt-workarounds vulkan-tools xwaylandvideobridge borgbackup fontawesome-fonts-all
-
-#dnf5 copr -y disable ublue-os/packages
+dnf5 copr -y disable ublue-os/packages
 dnf5 copr -y disable solopasha/hyprland
 
 #### Example for enabling a System Unit File
-# systemctl enable podman.socket
+
+systemctl enable podman.socket
